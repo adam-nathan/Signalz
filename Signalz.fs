e285ac830e66df1bdd660512a06416a769c376e4
adam-nathan <58827455+adam-nathan@users.noreply.github.com>
2021-05-04 10:15:54 +0300

    Add signals type

--- Signalz.fs
+++ Signalz.fs
@@ -1,1 +1,55 @@
 ﻿namespace Signalz
+open System.Collections.Generic
+
+module internal Internals = 
+    type Signal = 
+        abstract member ReCompute : unit -> unit
+        abstract member Observe : Signal -> unit
+        abstract member Ignore : Signal -> unit
+
+    type NoSignal () =
+        interface Signal with 
+            member __.ReCompute () = ()
+            member __.Observe _ = () 
+            member __.Ignore _ = () 
+
+    let caller = new DynamicVariable<Signal>(NoSignal())
+
+open Internals
+type Signal<'T when 'T : equality> (expr: unit -> 'T) as this = 
+    let mutable expr = expr
+    let mutable value = expr ()
+    let observers = new HashSet<Signal>()
+    let observed = new ResizeArray<Signal>()
+
+    let recompute () = 
+        for s in observed do s.Ignore this
+        observed.Clear()
+
+        let newValue = caller.WithValue(this, fun () -> expr ())
+        if value <> newValue then
+            value <- newValue
+
+            let observers' = Seq.toList observers
+            observers.Clear()
+            for observer in observers' do 
+                observer.ReCompute ()
+
+    do recompute ()
+
+    member __.Value =  
+        ignore <| observers.Add caller.Value
+        caller.Value.Observe this
+        value
+    member private __.Update (expr':unit -> 'T) = 
+        expr <- expr'
+        recompute ()
+
+    static member (<~) (s:Signal<'T>, v:'T) = s.Update (fun () -> v)
+
+    interface Signal with 
+        member __.ReCompute () = recompute ()
+        member __.Observe s = ignore <| observed.Add s
+        member __.Ignore s = ignore <| observed.Remove s
+
+type 'a signal when 'a : equality = Signal<'a>      // alias 
